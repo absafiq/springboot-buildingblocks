dc5c75a79e5f89c907a572205ab9a0be2d600590
shafeeq.bari <shafeeq.abdul@telusinternational.com>
2021-05-20 15:46:18 -0600

    new api

---
+++ src/main/java/com/learn/TestController.java
@@ -0,0 +1,14 @@
+package com.learn;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping(method = RequestMethod.GET,path = "/hello")
+    public String hellowor() {
+        return "Testing Hello world";
+    }
+}
